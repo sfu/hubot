7dc0fa1043cc94c855f1f915cfc6e5e78030ede3
Graham Ballantyne <hello@grahamballantyne.com>
2016-01-15 14:35:11 -0800

    fix path in start-hubot.sh

--- start-hubot.sh
+++ start-hubot.sh
@@ -3,6 +3,6 @@
 # Simple script to keep hubot running
 # This script will restart it if it dies, and log why it died.
 
-source /var/nodeapps/hubot/.hubotrc
-forever start --uid 'hubot' -a -l /var/nodeapps/hubot/hubot.log -c coffee node_modules/.bin/hubot -a slack
+source /home/nodeuser/apps/hubot/.hubotrc
+forever start --uid 'hubot' -a -l /home/nodeuser/apps/hubot/hubot.log -c coffee node_modules/.bin/hubot -a slack
 
